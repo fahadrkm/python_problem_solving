2fdf7fa9dc793387a5858d0cdbbfd34fd6dbaa35
fahadrkm <fahadrkm@gmail.com>
2025-01-16 15:18:37 +0530

    string validate problem solution

---
+++ string_validate.py
@@ -0,0 +1,16 @@
+s = input()
+
+# Check for alphanumeric characters
+print(any(c.isalnum() for c in s))
+
+# Check for alphabetical characters
+print(any(c.isalpha() for c in s))
+
+# Check for digits
+print(any(c.isdigit() for c in s))
+
+# Check for lowercase characters
+print(any(c.islower() for c in s))
+
+# Check for uppercase characters
+print(any(c.isupper() for c in s))
