2ad41421c324ef10e2015bf66c3b06201607b3ef
fahadrkm <fahadrkm@gmail.com>
2025-02-04 23:46:16 +0530

    function

--- functionglobal.py
+++ functionglobal.py
@@ -1,7 +1,59 @@
-val=30
-def hi():
- val=40
- print(val)
+# val=30
+# def hi():
+#  val=40
+#  print(val)
  
-print(val)
-hi()
+# print(val)
+# hi()
+
+
+def sample():
+    t="hi"
+    print(t)
+    a=123
+    return a
+    
+    
+# sample()
+# print(sample())
+"_______________________________________"
+
+
+
+
+# def add(a,b):
+#     s="sweet"
+#     print("hi")
+#     return a+b ,s
+
+# print(add(8,9))
+
+# res=add(10,20)
+# res
+
+# print(add(5,6))
+"op:"
+# hi
+# (17, 'sweet')
+# hi
+# hi
+# (11, 'sweet')
+"""___________________"""
+
+
+# def greet(name):
+    
+#     print(f"hi {name}")
+
+# greet(name=5)
+
+"op:"
+"hi 5"
+"___________________"
+import math
+
+def multi(*ag):
+    return math.prod(ag)
+
+res=multi(2,3,4)
+
